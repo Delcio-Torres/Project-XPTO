af7fa8c62689977208fa5a5e7f86fbfc198eda11
Alex Alves <alex.silva@plus-it.com.br>
2021-02-09 16:08:33 -0300

    Remoção de assinatura/certificado
    Separando o que é Path (caminho do arquivo) e Data Source (banco de dados)
    
    Separando o que é Path (caminho) e o que é file (arquivo)
    
    Verificando se existe o diretório e/ou o arquivo.
    Criar o que não existe
    Ao criar o arquivo, fechá-lo imediatamente para a aplicação poder manipulá-lo
    
    Realizando verificação se conexão já não está fechada
    
    Remoção de variável não utilizada
    
    Modificando tipos de acessores de path e file para private:
    - Apenas a classe que pode alterá-los/manipulá-los
    Adicionando o arquivo do banco de dados como constante:
    - É entendível que o nome do arquivo nunca mudará
    
    Adicionando modificadores readonly para impedir que campos não sejam modificados no decorrer da classe/processo
    
    Banco é criado sem nenhuma tabela prévia existente:
    - Criação de método para criar tabelas necessárias
    - Invocação do méotodo de criação d etabelas via Construtor
    Obs.: Tabelas são criadas caso não existam
    
    Separação do que é conexão com banco/start e do que é operações
    Obs.: No select por ID, passar o ID como parameter para evitar SQLInjection

---
+++ Controller/Operations.cs
@@ -0,0 +1,120 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public class Operations
+    {
+        private readonly Connection _connection;
+
+        public Operations()
+        {
+            _connection = new Connection();
+        }
+
+        public void InsertWorker(Worker worker)
+        {
+            var sql = $"INSERT INTO TB_Worker (Nome, Email, Cpf, DataNascimento) VALUES (@Nome, @Email, @Cpf, @DataNascimento)";
+            var parameters = new Dictionary<string, object>
+            {
+                { "@Nome", worker.Nome },
+                { "@Email", worker.Email },
+                { "@Cpf", worker.Cpf },
+                { "@DataNascimento", worker.DataNascimeto}
+            };
+
+            ExecuteCommand(sql, parameters);
+        }
+
+        public List<Worker> GetAllWorker()
+        {
+            string sql = "SELECT Id_Worker, Nome, Email, Cpf, DataNascimento FROM TB_Worker order by DataNascimento";
+
+            var result = ExecuteQuery(sql);
+
+            return result.ToList();
+        }
+
+        public Worker GetWorker(int id)
+        {
+            string sql = $"SELECT Id_Worker, Nome, Email, Cpf, DataNascimento FROM TB_Worker WHERE ID_Worker = @Id_Worker ORDER BY Nome";
+            var parameters = new Dictionary<string, object>
+            {
+                { "@Id_Worker", id }
+            };
+
+            var result = ExecuteQuery(sql, parameters);
+
+            return result.First();
+        }
+
+        private void ExecuteCommand(string sql, IDictionary<string, object> parameters)
+        {
+            _connection.OpenDB();
+
+            var cmd = _connection.Conn.CreateCommand();
+
+            cmd.CommandText = sql;
+            cmd.Prepare();
+
+            if(parameters != null && parameters.Any())
+            {
+                foreach (var parameter in parameters)
+                {
+                    cmd.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                }
+            }
+
+            cmd.ExecuteNonQuery();
+
+            _connection.CloseDB();
+        }
+
+        private IEnumerable<Worker> ExecuteQuery(string sql, IDictionary<string, object> parameters = null)
+        {
+            _connection.OpenDB();
+
+            var cmd = _connection.Conn.CreateCommand();
+
+            cmd.CommandText = sql;
+            cmd.Connection = _connection.Conn;
+
+            if (parameters != null && parameters.Any())
+            {
+                cmd.Prepare();
+                foreach (var parameter in parameters)
+                {
+                    cmd.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                }
+            }
+
+            var dataReader = cmd.ExecuteReader();
+
+            var workers = new List<Worker>();
+
+            while (dataReader.Read())
+            {
+                workers.Add
+                (
+                    new Worker
+                    {
+                        Id = Convert.ToInt32(dataReader["Id_Worker"]),
+                        Cpf = dataReader["Cpf"].ToString(),
+                        DataNascimeto = (DateTime)dataReader["DataNascimento"],
+                        Email = dataReader["Email"].ToString(),
+                        Nome = dataReader["Nome"].ToString()
+                    }
+                );
+            }
+
+            _connection.CloseDB();
+
+            return workers;
+        }
+    }
+}
